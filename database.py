a9ebe1584bd0dcdaebb340950e70084ba94b8e42
Salih <Salihyanikbsyb@gmail.com>
2026-03-25 22:28:41 +0300

    requirements guncellendi ve postgres baglandi

--- database.py
+++ database.py
@@ -1,13 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./ekg.db"  # dosya aynı klasörde oluşur
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
